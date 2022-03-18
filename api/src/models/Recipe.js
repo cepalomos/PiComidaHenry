354569e2a79c348ced557745055c2807e1a12584
Carlos Palomo Serna <cepalomos@outlook.es>
2022-03-18 17:38:23 -0500

    modelos terminados v1

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,26 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
+    id:{
+      type:DataTypes.UUID,
+      defaulValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
+    },
+    summary:{
+      type: DataTypes.TEXT,
+    },
+    score:{
+      type: DataTypes.INTEGER
+    },
+    healthScore:{
+      type: DataTypes.INTEGER
+    },
+    steps:{
+      type: DataTypes.TEXT
     },
   });
 };
